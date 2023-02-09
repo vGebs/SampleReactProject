9d762e3a9cc080648afd2fd4db9598d4badeed53
vGebs <vaughn.geber33@gmail.com>
2023-02-09 03:04:00 -0600

    complete cta

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -3,8 +3,14 @@ import './cta.css'
 
 const CTA = () => {
     return (
-        <div>
-            CTA
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-content">
+                <p>Request early access to get started</p>
+                <h3>Buncha nonsense here lmao, just filler text to look gucci</h3>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
         </div>
     )
 };
