48711c82b1d631bac9619fefae77f0d2d49d0fa5
vGebs <vaughn.geber33@gmail.com>
2023-02-09 15:59:46 -0600

    complete footer

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import './footer.css'
+import gpt3Logo from "../../assets/logo.svg"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="gpt3__footer section__padding">
+            <div className="gpt3__footer-heading">
+                <h1 className="gradient__text">Do you want to step into the future before the others?</h1>
+            </div>
+            <div className="gpt3__footer-btn">
+                <p>Request Early Access</p>
+            </div>
+            <div className="gpt3__footer-links">
+                <div className="gpt3__footer-links_logo">
+                    <img src={gpt3Logo} alt="logo"/>
+                    <p>Here is bunch of filler text to make the page look good</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Links</h4>
+                    <p>Overons</p>
+                    <p>Social Media</p>
+                    <p>Counters</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Company</h4>
+                    <p>Terms and Conditions</p>
+                    <p>Privacy Policy</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Get in Touch</h4>
+                    <p>123 Main street</p>
+                    <p>123-456-7890</p>
+                    <p>info@getSomeInfo.com</p>
+                </div>
+                <div className="gpt3__footer-copyright">
+                    <p>2021 GPT-3. All Rights Reserved</p>
+                </div>
+            </div>
         </div>
     )
 };
