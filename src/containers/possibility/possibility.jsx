2548e421dcf694e2f455cc76fd0b08b0fcd2ff12
vGebs <vaughn.geber33@gmail.com>
2023-02-09 02:47:11 -0600

    complete possibility

--- src/containers/possibility/possibility.jsx
+++ src/containers/possibility/possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './possibility.css'
+import possibilityImage from "../../assets/possibility.png"
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
+        <div className="gpt3__possibility section__padding" id="possibility">
+            <div className="gpt3__possibility-image">
+                <img src={possibilityImage}/>
+            </div>
+            <div className="gpt3__possibility-content">
+                <h4>Request early access to get started</h4>
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                <p>afe aewfawea we f awe f aw e  gt srgt gs rt gs erg eb rtb rdt b trd n ytn ty dg bdfg  bvsdfsefdv</p>
+                <h4>Request early access to get started</h4>
+            </div>
         </div>
     )
 };
