f7525bcffb3a8f719860d2108297900c06fb9cfe
vGebs <vaughn.geber33@gmail.com>
2023-02-08 20:25:22 -0600

    folder setup

---
+++ src/containers/whatChatGPT3/whatChatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatChatGPT3.css'
+
+const WhatChatGPT3 = () => {
+    return (
+        <div>
+            WhatChatGPT3
+        </div>
+    )
+};
+
+export default WhatChatGPT3;
