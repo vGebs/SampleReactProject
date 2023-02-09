a7951922d4a6b5f187a340aad2cb2e15662fd8d1
vGebs <vaughn.geber33@gmail.com>
2023-02-09 01:47:52 -0600

    complete whatChatGPT3 and feature

--- src/components/feature/feature.jsx
+++ src/components/feature/feature.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title, text}) => {
     return (
-        <div>
-            Feature
+       <div className="gpt3__features-container__feature">
+            <div className="gpt3__features-container__feature-title">
+                <h1>{title}</h1>
+            </div>
+            <div className="gpt3__features-container_feature-text">
+                <p>{text}</p>
+            </div>
         </div>
     )
 };
--- src/containers/whatChatGPT3/whatChatGPT3.jsx
+++ src/containers/whatChatGPT3/whatChatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatChatGPT3.css'
 
 const WhatChatGPT3 = () => {
     return (
-        <div>
-            WhatChatGPT3
+        <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+            <div className="gpt__whatgpt3-feature">
+                <Feature title="What is ChatGPT-3?" text="Here is a bunch of filler text to tell you about ChatGPT-3 and how cool it is"/>
+            </div>
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className="gradient__text">The possibilites are beyond your imagination</h1>
+                <p>Explore this library</p>
+            </div>
+            <div className="gpt3__whatgpt3-container">
+                <Feature title="ChatBots" text="Some more text about ChatGPT-3 and how it is a chatbot"/>
+                <Feature title="Knowledgebase" text="It has a wide knowledgebase, you can ask it almost anyting!"/>
+                <Feature title="Education" text="You can learn using ChatGPT, it has all of the answers"/>
+            </div>
         </div>
     )
 };
